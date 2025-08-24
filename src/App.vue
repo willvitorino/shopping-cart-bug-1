92977a2bd5b124960da17d9b00def351652bb370
Will Vitorino <jose.william@softplan.com.br>
2025-08-24 13:18:38 -0300

    ✨ Adiciona rodapé com créditos do desenvolvedor
    **Alterações:**
    - **src/App.vue**: Adiciona layout em coluna e rodapé com link para o GitHub do desenvolvedor, incluindo hover effects.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@ import ShoppingCart from './components/ShoppingCart.vue';
 </script>
 <template>
   <div
-    class="flex items-center justify-center dark:text-white h-screen w-screen"
+    class="flex flex-col items-center justify-center dark:text-white h-screen w-screen"
   >
     <ShoppingCart />
+
+    <span class="text-sm mt-8">
+      by <a href="https://github.com/willvitorino" target="_blank" class="text-blue-500 hover:underline">William Vitorino</a>
+    </span>
   </div>
 </template>
